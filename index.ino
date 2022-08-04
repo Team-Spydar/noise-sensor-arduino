23b5b29e0135c23f4e4072817602f04f0701fd9c
bejoy obiora <bejoyobiora@gmail.com>
2022-08-03 22:22:13 -0700

    add void setup

--- index.ino
+++ index.ino
@@ -1,7 +1,15 @@
 // variables
+ #define noise_sensor 16
+int BUZZER  = 7;
+int discussion_sounds;
+int noisy_sounds;
 
 // setup
-
+void setup(){
+pinMode (noise_sensor, INPUT);
+pinMode (BUZZER, OUTPUT);
+Serial.begin(9600);
+}
 // loop
 
 // functions
